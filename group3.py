bce7d932d93bc557e91ebb458b782792a6156094
Amuthu98 <45287815+Amuthu98@users.noreply.github.com>
2020-12-09 14:53:26 +0100

    Update group3.py

--- group3.py
+++ group3.py
@@ -3,13 +3,20 @@ from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
  
+default_dag_args = {
+    'owner': 'group3',
+    'start_date': datetime.now(),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
 dag = DAG(
-dag_id = "group3",
-start_date = "2020-12-09",
-interval = "1 day")
+    dag_id='group3_dag',
+    schedule_interval = timedelta(days=1),
+    default_args=default_dag_args
+)
  
 task1 = BashOperator(
-task_id = "group3_dag",
+task_id = "group3_dag1",
 bash_command = "echo hello world",
 dag = dag)
  
